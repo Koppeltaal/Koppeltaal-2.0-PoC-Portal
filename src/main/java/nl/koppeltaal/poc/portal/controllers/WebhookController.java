4a052fca7a0b6cfa88d2b44a8b0c4d24c8b9df35
Joris Scharp <jorisscharp@gmail.com>
2021-05-10 17:08:19 +0200

    KOP-95: Implemented a webhook endpoint that will send a welcome email to new patients. This is a basic example that uses a Subscription.payload to give quickly access the email address. This should normally be done by requesting the resource from the FHIR server.

---
+++ src/main/java/nl/koppeltaal/poc/portal/controllers/WebhookController.java
@@ -0,0 +1,30 @@
+package nl.koppeltaal.poc.portal.controllers;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import nl.koppeltaal.poc.portal.service.EmailService;
+import org.hl7.fhir.r4.model.Patient;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("webhook")
+public class WebhookController {
+
+  private final EmailService emailService;
+
+  private final IParser jsonParser = FhirContext.forR4().newJsonParser();
+
+  public WebhookController(EmailService emailService) {
+    this.emailService = emailService;
+  }
+
+  @PutMapping(value = "patient/**", consumes = "application/fhir+json;charset=UTF-8")
+  public void patientWebhook(@RequestBody String patientPayload) {
+
+    final Patient patient = jsonParser.parseResource(Patient.class, patientPayload);
+
+    emailService.sendWelcomeEmail(patient);
+  }
+
+}
