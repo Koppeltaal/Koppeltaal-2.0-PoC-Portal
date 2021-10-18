c923af338cf5821fdb681bc4c30cda80456d3336
Joris Scharp <jorisscharp@gmail.com>
2021-10-18 20:33:11 +0200

    Made it so that the patient can only see  their own CareTeams. This isn't very safe code as Patients can assign CareTeams to themselves when knowing an identifier, but this is POC code..

--- src/main/java/nl/koppeltaal/poc/portal/controllers/CareTeamController.java
+++ src/main/java/nl/koppeltaal/poc/portal/controllers/CareTeamController.java
@@ -8,11 +8,31 @@
 
 package nl.koppeltaal.poc.portal.controllers;
 
+import ca.uhn.fhir.rest.gclient.ICriterion;
+import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
+import com.auth0.jwk.JwkException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import nl.koppeltaal.spring.boot.starter.smartservice.dto.CareTeamDto;
 import nl.koppeltaal.spring.boot.starter.smartservice.dto.CareTeamDtoConverter;
+import nl.koppeltaal.spring.boot.starter.smartservice.dto.PatientDto;
+import nl.koppeltaal.spring.boot.starter.smartservice.dto.RelatedPersonDto;
 import nl.koppeltaal.spring.boot.starter.smartservice.service.fhir.CareTeamFhirClientService;
 import org.hl7.fhir.r4.model.CareTeam;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Practitioner;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.RelatedPerson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +44,46 @@ public class CareTeamController extends BaseResourceController<CareTeamDto, Care
 
 	public CareTeamController(CareTeamFhirClientService fhirClientService, CareTeamDtoConverter dtoConverter) {
 		super(fhirClientService, dtoConverter);
+	}
+
+	@GetMapping
+	public List<CareTeamDto> list(HttpSession httpSession) throws IOException, JwkException {
+
+		Object user = httpSession.getAttribute("user");
+		if (user instanceof Practitioner) {
+			return super.list(httpSession);
+		} else if (user instanceof Patient) {
+			List<CareTeamDto> rv = new ArrayList<>();
+
+			String reference = getUserReference((Patient) user);
+			ICriterion<ReferenceClientParam> criterion = CareTeam.SUBJECT.hasId(reference);
+			List<CareTeam> relatedPersons = fhirClientService.getResources( criterion);
+			for (CareTeam careTeam : relatedPersons) {
+				rv.add(dtoConverter.convert(careTeam));
+			}
+			return rv;
+		} else {
+			return Collections.emptyList();
+		}
+	}
+
+	@PutMapping
+	public CareTeamDto put(HttpSession httpSession, HttpServletRequest request, @RequestBody CareTeamDto dto) throws IOException {
+
+		Object user = httpSession.getAttribute("user");
+
+		if(!(user instanceof Patient)) {
+			throw new SecurityException("Only Patients are allowed to create CareTeams");
+		}
+
+		final String userReference = getUserReference((Patient) user);
+		final CareTeam convertedCareTeam = dtoConverter.convert(dto);
+		convertedCareTeam.setSubject(new Reference(userReference));
+		return dtoConverter.convert(fhirClientService.storeResource(convertedCareTeam));
+	}
+
+	private String getUserReference(Patient user) {
+		IdType id = user.getIdElement();
+		return id.getResourceType()  + "/" + id.toUnqualifiedVersionless().getIdPart();
 	}
 }
